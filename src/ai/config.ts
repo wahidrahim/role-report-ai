1d52912aba697600e95d0738eb875bf7b957b3a0
Wahid Rahim <wahidrahim@gmail.com>
2025-12-14 22:42:39 -0500

    refactor: update AI model selection based on environment

--- src/ai/config.ts
+++ src/ai/config.ts
@@ -1,5 +1,4 @@
-import { LanguageModel } from 'ai';
 import { ollama } from 'ollama-ai-provider-v2';
 
-export const model = ollama('qwen3:30b');
-// export const model: LanguageModel = 'openai/gpt-4o';
+export const model =
+  process.env.NODE_ENV === 'development' ? ollama('qwen3:30b') : 'openai/gpt-4o-mini';
