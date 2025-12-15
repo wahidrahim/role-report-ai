bb91a748378f877a6bf2caebb3ed04cf73bb2155
Wahid Rahim <wahidrahim@gmail.com>
2025-12-14 22:54:37 -0500

    chore: remove unused files

--- src/core/utils.ts
+++
@@ -1,6 +0,0 @@
-const encoder = new TextEncoder();
-
-export const sendDataFn =
-  (controller: ReadableStreamDefaultController) => (type: string, data: unknown) => {
-    controller.enqueue(encoder.encode(`data: ${JSON.stringify({ type, data })}\n\n`));
-  };
